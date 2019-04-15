8b52b671353c30d322e8d64f6f5f05f4363de275
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2019-04-15 12:20:11 +0200

    Add database PDO connection

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,14 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Database settings
+        'db' => [
+            'host' => '127.0.0.1',
+            'dbname' => 'Ticketone',
+            'port' => '3306',
+            'user' => 'andrea',
+            'pass' => 'slacky91'
+        ],
     ],
 ];
